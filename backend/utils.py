c103550b34488b71aba21b60e73521e3efa3f5cf
Mikkeep <mikke.pennanen@gmail.com>
2022-10-16 17:58:59 +0300

    Utility methods for database operations in one file

---
+++ backend/utils.py
@@ -0,0 +1,36 @@
+"""
+Utility methods for Flask
+"""
+
+import sqlite3
+from flask import g
+from .constants import DB_LOCATION
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            DB_LOCATION,
+            detect_types=sqlite3.PARSE_DECLTYPES,
+        )
+        g.db.row_factory = dict_factory
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+
+
+def dict_factory(cursor, row):
+    col_names = [col[0] for col in cursor.description]
+    return {key: value for key, value in zip(col_names, row)}
+
+
+def fetch_item(command):
+    user = get_db().execute(command).fetchone()
+    close_db()
+    return user
