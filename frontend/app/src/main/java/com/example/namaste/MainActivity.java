cebb9c0d1609ae3931292b2964b94801f1c2d49c
[suturri] <lsuutari19@student.oulu.fi>
2022-10-20 15:06:36 +0300

    color changes and implementation of scrollview restaurant page

--- frontend/app/src/main/java/com/example/namaste/MainActivity.java
+++ frontend/app/src/main/java/com/example/namaste/MainActivity.java
@@ -2,20 +2,32 @@ package com.example.namaste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton imgBtn;
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
 
@@ -24,15 +36,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgBtn = findViewById(R.id.idImgBtn);
+        // placeholder restaurant name array for buttons (replace with results of a query of backend)
+        String[] restNames = new String[]{"Bob's Burgers", "Alice's Apples", "Peter's Pies", "DjRonalds", "Hasburger", "Segway", "Taco Ball"};
+        String[] restDesc = new String[]{"Best burgers!", "Delicious Apples", "Mmm...pies", "What is McDonalds?", "Its in the game", "Eat fresh", "TexMex restaurant"};
 
-        imgBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // on below line we are displaying a toast message.
-                Toast.makeText(MainActivity.this, "This is a image button", Toast.LENGTH_SHORT).show();
-            }
-        });
+        // importing linearlayout for buttons
+        // nested in a scrollview for scrolling
+        LinearLayout restaurantBoard = (LinearLayout) findViewById(R.id.l1);
+        // set the size of the individual buttons
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(1000, 300);
+        for (int i = 0; i < restNames.length; i++) {
+            Button btn = new Button(this);
+            btn.setId(i);
+            Spannable span = new SpannableString(restNames[i]+"\n-"+restDesc[i]);
+            btn.setText(span);
+            btn.setLayoutParams(lp);
+            btn.setOnClickListener(restaurantButtonListener);
+            // sets the icon for button
+            Drawable icon = getApplicationContext().getResources().getDrawable(R.drawable.burger);
+            Bitmap bitmap = ((BitmapDrawable) icon).getBitmap();
+            Drawable resizedIcon = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, 200, 200, true));
+            btn.setCompoundDrawablesWithIntrinsicBounds(null, null, resizedIcon, null);
+            restaurantBoard.addView(btn);
+        }
 
         // drawer layout instance to toggle the menu icon to open
         // drawer and back button to close drawer
@@ -48,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-
     // click listener callback
     // to open and close the navigation
     // drawer when the icon is clicked
@@ -60,5 +85,16 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    // click listener for the restaurant buttons
+    private View.OnClickListener restaurantButtonListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            // check which button was clicked
+            Button btn = (Button) v;
+            Toast.makeText(MainActivity.this, "Clicked button " + v.getId(), Toast.LENGTH_SHORT).show();
+        }
+    };
 
 }
