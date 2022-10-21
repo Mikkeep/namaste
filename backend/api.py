71f3503330665ed3353a47ca56ee3fc327c1fa8b
Mikkeep <mikke.pennanen@gmail.com>
2022-10-21 10:17:11 +0300

    Added resource for logout and modified restaurants resource name

--- backend/api.py
+++ backend/api.py
@@ -4,12 +4,13 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .resources.user import UserLogin, UserRegister
-from .resources.restaurant import Restaurant
+from .resources.user import UserLogin, UserRegister, UserLogout
+from .resources.restaurant import Restaurants
 
 API_BP = Blueprint("api", __name__, url_prefix="/api")
 API = Api(API_BP)
 
 API.add_resource(UserLogin, "/users/login/")
+API.add_resource(UserLogout, "/users/logout/")
 API.add_resource(UserRegister, "/users/register/")
-API.add_resource(Restaurant, "/restaurant/all")
+API.add_resource(Restaurants, "/restaurant/all")
