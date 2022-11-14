48910664c2747b01caedf0274d95b91a0fcf9c9b
pintti <allu.tuovinen@gmail.com>
2022-11-14 19:23:00 +0200

    FUCK

--- backend/resources/file_handling.py
+++ backend/resources/file_handling.py
@@ -1,6 +1,6 @@
 """ Methods for Files """
 
-from flask import request, Response, session, send_from_directory, send_file
+from flask import send_file
 from flask_restful import Resource
 from ..constants import JSON
 
@@ -8,5 +8,5 @@ from ..constants import JSON
 class getFile(Resource):
     
     def get(self):
-        return send_file("./file.pdf", "application/pdf", as_attachment=True)
+        return send_file(".\\resources\\file.pdf", "application/pdf", as_attachment=True)
 
