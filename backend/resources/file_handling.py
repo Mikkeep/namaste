c44c3063f22d02345f4d71a1c6a768fbd0609e95
pintti <allu.tuovinen@gmail.com>
2022-11-16 13:28:04 +0200

    check os version

--- backend/resources/file_handling.py
+++ backend/resources/file_handling.py
@@ -1,7 +1,7 @@
 """ Methods for Files """
 
 from flask import send_file
-import os
+import platform
 from flask import request, Response, session, send_from_directory, send_file
 from flask_restful import Resource
 from ..constants import JSON
@@ -10,4 +10,7 @@ from ..constants import JSON
 class getFile(Resource):
     
     def get(self):
-        return send_file(".\\resources\\file.pdf", "application/pdf", as_attachment=True)
+        if platform.system() == "Linux" or platform.system() == "Darwin":
+            return send_file("./resources/file.pdf", "application/pdf", as_attachment=True)
+        elif platform.system() == "Windows":
+            return send_file(".\\resources\\file.pdf", "application/pdf", as_attachment=True)
