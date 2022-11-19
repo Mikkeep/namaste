8dac667b55681c825864da6198a85e6838f4e048
Mikkeep <mikke.pennanen@gmail.com>
2022-11-19 14:57:53 +0200

    Added support for TLS connections and reformatted code to more simple version

--- frontend/app/src/main/java/com/example/namaste/OkHttpPostRequest.java
+++ frontend/app/src/main/java/com/example/namaste/OkHttpPostRequest.java
@@ -3,12 +3,18 @@ package com.example.namaste;
 import android.os.StrictMode;
 import android.util.Log;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
 
-import okhttp3.Headers;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.ConnectionSpec;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -17,85 +23,98 @@ import okhttp3.Response;
 
 public class OkHttpPostRequest {
 
-    public Response doPostRequest(String one, String two, String three, String four, String five, String reqType) {
+    // from GitHub Gist:
+    // https://gist.github.com/preethamhegdes/fcab7bced52bf2520994ce232f2102ed
+    private static OkHttpClient.Builder configureToIgnoreCertificate(OkHttpClient.Builder builder) {
+        try {
+
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                                throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                                throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            Log.d("OKHTTP3", "Exception while configuring IgnoreSslCertificate");
+        }
+        return builder;
+    }
+
+    public Response doPostRequest(String url, String body, String cookie) {
+
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                 .permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        Log.d("OKHTTP3", "POST function called");
-        OkHttpClient client = new OkHttpClient();
-        MediaType JSON = MediaType.parse("application/json;charset=utf-8");
-        JSONObject data = new JSONObject();
-        String url = "http://10.0.2.2:5000/api/" + reqType;
+        //        OkHttpClient client = new OkHttpClient.Builder()
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+//                .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
+//                .followSslRedirects(true)
+//                .followRedirects(true)
+//                .build();
 
-        if(reqType.contains("users/login") || reqType.contains("users/register")) {
-            try {
-                data.put("username", one);
-                data.put("password", two);
-            } catch (JSONException e) {
-                Log.d("OKHTTP3", "JSON exception in login.");
-                e.printStackTrace();
-            }
+//        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder = configureToIgnoreCertificate(builder);
+
+        OkHttpClient client = null;
+        client = builder
+                .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
+                .followSslRedirects(true)
+                .followRedirects(true)
+                .build();
+
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        String base_url = "http://10.0.2.2:5000/api/";
+        url = base_url + url;
+
+        if (body == null) {
+            body = "";
         }
-
-        if(reqType.contains("order")) {
-            try {
-                data.put("rest_id", one);
-                data.put("item_id", two);
-                data.put("amount", three);
-                data.put("description", four);
-            } catch (JSONException e) {
-                Log.d("OKHTTP3", "JSON exception in order.");
-                e.printStackTrace();
-            }
-        }
-
-        Log.d("data to be posted:", data.toString());
-
-        RequestBody body = RequestBody.create(JSON, data.toString());
-        Log.d("OKHTTP3", "Request body created.");
-        Log.d("OKHTTP3 RB:", body.toString());
+        RequestBody reqBody = RequestBody.create(JSON, body);
 
         Request newReq = null;
-        // Assemble headers and body for login/register request
-        if(reqType.contains("users/login") || reqType.contains("users/register")) {
-            newReq = new Request.Builder()
-                    .url(url)
-                    .post(body)
-                    .build();
-        }
-        // Assemble headers and body for logout request
-        if(reqType.contains("users/logout")) {
-            newReq = new Request.Builder()
-                    .addHeader("Cookie", "session=" + five)
-                    .url(url)
-                    .post(body)
-                    .build();
-        }
-        // Assemble headers and body for order request
-        if(reqType.contains("order")) {
-            newReq = new Request.Builder()
-                    .addHeader("Cookie", "session=" + five)
-                    .url(url)
-                    .post(body)
-                    .build();
-        }
 
-        Log.d("response is:", newReq.toString());
+        newReq = new Request.Builder()
+                .addHeader("Cookie", "session=" + cookie)
+                .url(url)
+                .post(reqBody)
+                .build();
+
         Response response = null;
         try {
             response = client.newCall(newReq).execute();
-            Log.d("OKHTTP3", "Request done, got the response");
-            if(response.toString().contains("200")) {
-                Log.d("got here", "got here!");
-                if(reqType.contains("users")) {
-                    Headers headers = response.headers();
-                    String sId = headers.get("Set-Cookie");
-                    Log.d("Set-cookie: ", sId);
-                }
-            }
         } catch (IOException e) {
             Log.d("OKHTTP3", "Exception while doing request.");
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            Log.d("OKHTTP3", "No request body");
             e.printStackTrace();
         }
         Log.d("OKHTTP3", String.valueOf(response));
