561ec9378c32279e26877c50a7ec49af4d466bc4
Mikkeep <mikke.pennanen@gmail.com>
2022-11-19 14:57:15 +0200

    Reformatted code to more simple format and added TLS support, while maintaining support for HTTP connection simultaneously

--- frontend/app/src/main/java/com/example/namaste/OkHttpGetRequest.java
+++ frontend/app/src/main/java/com/example/namaste/OkHttpGetRequest.java
@@ -4,20 +4,84 @@ import android.os.StrictMode;
 import android.util.Log;
 
 import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class OkHttpGetRequest {
-    public Response doGetRequest(String cookie) {
+
+    // from GitHub Gist:
+    // https://gist.github.com/preethamhegdes/fcab7bced52bf2520994ce232f2102ed
+    private static OkHttpClient.Builder configureToIgnoreCertificate(OkHttpClient.Builder builder) {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                                throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                                throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            Log.d("OKHTTP3", "Exception while configuring IgnoreSslCertificate");
+        }
+        return builder;
+    }
+
+    public Response doGetRequest(String url, String cookie) {
+        OkHttpClient client = null;
+
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                 .permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        Log.d("OKHTTP3", "GET function called");
-        OkHttpClient client = new OkHttpClient();
-        String url = "http://10.0.2.2:5000/api/restaurant/all";
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+        builder = configureToIgnoreCertificate(builder);
+        client = builder
+                .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
+                .followSslRedirects(true)
+                .followRedirects(true)
+                .build();
+
+        String baseUrl = "http://10.0.2.2:5000/api/";
+        url = baseUrl + url;
 
         Request newReq = new Request.Builder()
                 .addHeader("Cookie", "session=" + cookie)
