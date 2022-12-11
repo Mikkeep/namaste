bf68170b6ca4fa2406cf164407971d0eb6dedfff
Mikkeep <mikke.pennanen@gmail.com>
2022-12-11 10:43:58 +0200

    Root & Emulator checks added Root check is function within the class Emulator check is activated if android manifest has hardware acceleration turned on

--- frontend/app/src/main/java/com/example/namaste/LoginActivity.java
+++ frontend/app/src/main/java/com/example/namaste/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.namaste;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -13,8 +15,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
 
 import okhttp3.Response;
 
@@ -23,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -37,6 +40,32 @@ public class LoginActivity extends AppCompatActivity {
         assert actionBar != null;
         actionBar.show();
 
+        // Detect rooted phone by checking superuser binaries from the phone
+        if (rootPhone()) {
+            Toast.makeText(LoginActivity.this, "Rooting detected!", Toast.LENGTH_SHORT).show();
+            // end activity id phone is rooted
+            this.finish();
+        } else
+            Toast.makeText(LoginActivity.this, "No rooting detected", Toast.LENGTH_SHORT).show();
+
+        try {
+            ActivityInfo values = LoginActivity.this.getPackageManager().getActivityInfo(
+                    this.getComponentName(), 0);
+            if ((values.flags & ActivityInfo.FLAG_HARDWARE_ACCELERATED) != 0) {
+                Toast.makeText(LoginActivity.this, "Hardware accelerated", Toast.LENGTH_SHORT).show();
+                // Detects if an emulator is used and closes activity if one is detected
+                EmulatorDetection isEmulator = new EmulatorDetection();
+                if (isEmulator.inEmulator()) {
+                    Toast.makeText(LoginActivity.this, "Emulator detected!", Toast.LENGTH_SHORT).show();
+                    // end the activity if emulator detected with hardware acceleration turned on
+                    this.finish();
+                } else
+                    Toast.makeText(LoginActivity.this, "No emulator detected", Toast.LENGTH_SHORT).show();
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        
         loginButton.setOnClickListener(view -> {
             OkHttpPostRequest postReq = new OkHttpPostRequest();
 
@@ -51,7 +80,6 @@ public class LoginActivity extends AppCompatActivity {
             }
 
             Response response = postReq.doPostRequest("users/login", msg, null);
-
             if (response != null) {
                 if (response.toString().contains("200")) {
                     Toast.makeText(LoginActivity.this, "Login success!", Toast.LENGTH_SHORT).show();
@@ -85,4 +113,28 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    public boolean rootPhone() {
+        String[] superUserBinaries = new String[13];
+        superUserBinaries[0] = "/system/app/Superuser/Superuser.apk";
+        superUserBinaries[1] = "/system/app/Superuser.apk";
+        superUserBinaries[2] = "/sbin/su";
+        superUserBinaries[3] = "/system/bin/su";
+        superUserBinaries[4] = "/system/xbin/su";
+        superUserBinaries[5] = "/data/local/xbin/su";
+        superUserBinaries[6] = "/data/local/bin/su";
+        superUserBinaries[7] = "/system/sd/xbin/su";
+        superUserBinaries[8] = "/system/bin/failsafe/su";
+        superUserBinaries[9] = "/data/local/su";
+        superUserBinaries[10] = "/su/bin/su";
+        superUserBinaries[11] = "re.robv.android.xposed.installer-1.apk";
+        superUserBinaries[12] = "/data/app/eu.chainfire.supersu-1/base.apk";
+        File root;
+        for (String superUserBinary : superUserBinaries) {
+            root = new File(superUserBinary);
+            if (root.exists()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
